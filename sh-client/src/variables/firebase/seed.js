af05c9bcff5da54cf8cb2c04c05f148a929b4926
4d11 <aadilasim@hotmail.com>
2019-01-13 12:15:21 -0500

    Initialized seed code

---
+++ sh-client/src/variables/firebase/seed.js
@@ -0,0 +1,23 @@
+const med1 = {
+    name: 'Panadol',
+    dosage: 3,
+    startDate: Date("dsfasdfdsf"),
+    endDate: null
+}
+
+const med2 = {
+    ...
+}
+
+
+const patient1 = {
+    name: 'Alex Martin',
+    meds: [med1, med2]
+        ...
+}
+
+
+const doctor1 = {
+    name: 'Bob Sam',
+    patients: [patient1, ]
+}
